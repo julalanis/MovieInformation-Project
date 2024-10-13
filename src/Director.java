293920cb1bf1fad7c6603df89798031e6e2cd9da
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-13 00:08:33 -0500

    Update Director.java
    Cleaned up comments, submitted as is to professor

--- src/Director.java
+++ src/Director.java
@@ -1,53 +1,52 @@
 public class Director {
-	//------------------------ named variables ------------------------
-	private String name;
-	private int yearsOfDirecting;
-	private boolean oscarWinner;
-	
-	//------------------------ constructor that accepts nothing ------------------------
-	Director(){
-	}
-	
-	//------------------------ constructor that accepts named parameters ------------------------
-	Director(String name, int yearsOfDirecting, boolean oscarWinner){
-		this.name = name;
-		this.yearsOfDirecting = yearsOfDirecting;
-		this.oscarWinner = oscarWinner;
-	}
+    //named variables
+    private String name;
+    private int yearsOfDirecting;
+    private boolean oscarWinner;
 
-	//------------------------ get methods for 3 variables ------------------------
-	String getName() {
-		return name;
-	}
-	
-	int getYearsOfDirecting() {
-		return yearsOfDirecting;
-	}
-	
-	boolean getOscarWinner() {
-		return oscarWinner;
-	}
-	
-	//------------------------ set methods for 3 variables ------------------------
-	public void setName(String newName) {
-		this.name = newName;
-	}
-	
-	public void setYearsOfDirecting(int yearsDirecting) {
-		this.yearsOfDirecting = yearsDirecting;
-	}
-	
-	public void setOscarWinner(boolean isWinner) {
-		this.oscarWinner = isWinner;
-	}
-	
-	//------------------------ toString method to list variables------------------------
-	
-	public String toString() {
-		return "Director name: " + name + "\n" +
-			   "Years of Directing: " + yearsOfDirecting + "\n" +
-			   "Oscar Winner: " + oscarWinner;
-	}
+    // constructor that accepts nothing
+    Director(){
+    }
 
-	}
+    //constructor that accepts named parameters
+    Director(String name, int yearsOfDirecting, boolean oscarWinner){
+        this.name = name;
+        this.yearsOfDirecting = yearsOfDirecting;
+        this.oscarWinner = oscarWinner;
+    }
+
+    //get methods for 3 variables
+    String getName() {
+        return name;
+    }
+
+    int getYearsOfDirecting() {
+        return yearsOfDirecting;
+    }
+
+    boolean getOscarWinner() {
+        return oscarWinner;
+    }
+
+    //set methods for 3 variables
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setYearsOfDirecting(int yearsDirecting) {
+        this.yearsOfDirecting = yearsDirecting;
+    }
+
+    public void setOscarWinner(boolean isWinner) {
+        this.oscarWinner = isWinner;
+    }
+
+    //toString method to list variables
+    public String toString() {
+        return "Director name: " + name + "\n" +
+                "Years of Directing: " + yearsOfDirecting + "\n" +
+                "Oscar Winner: " + oscarWinner;
+    }
+
+}
 
